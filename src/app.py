f460b2758bff811aa6159c803e3e4a75a736dad9
rosaainz <al2173040136@azc.uam.mx>
2024-06-19 03:35:48 -0600

    add upload image with method POST

--- src/app.py
+++ src/app.py
@@ -3,8 +3,16 @@ from flask import Flask, jsonify, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from users import users
 
-app = Flask(__name__)
+UPLOAD_FOLDER = '/Users/rosasainz/Documents/uam/lsm_server/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+#verificar la extension del archivo
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET'])
 def ping():
@@ -30,8 +38,19 @@ def get_user_by_id(id):
             }
     return jsonify(return_value)
 
+@app.route('/upload', methods=['POST'])
+def upload_media():
+    if 'file' not in request.files:
+        return jsonify({'error':'media not provided'}), 400
+    file = request.files['file']
 
+    if file.filename == '':
+        return jsonify({'error': 'no file selected'}), 400
 
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return jsonify({'msg':'media uploaded successfully'}) 
 
 if __name__ == '__main__':  
     app.run(host="0.0.0.0", port=4000, debug=True)
