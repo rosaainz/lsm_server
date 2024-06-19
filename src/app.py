78f48b0f99f44598a602ac59d66d0d2338574d30
rosaainz <al2173040136@azc.uam.mx>
2024-06-18 19:12:57 -0600

    crear carpeta en donde se guardara la imagen

--- src/app.py
+++ src/app.py
@@ -1,7 +1,17 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask, jsonify, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 from users import users
 
+UPLOAD_FOLDER = 'uploads/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+#crear carpeta
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 @app.route('/', methods=['GET'])
 def ping():
@@ -10,6 +20,8 @@ def ping():
 @app.route('/users')
 def userHandler():
     return jsonify({"users": users})
+
+
 
 if __name__ == '__main__':  
     app.run(host="0.0.0.0", port=4000, debug=True)
